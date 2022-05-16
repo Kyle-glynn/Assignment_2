358d3946605869f2b0f819eab140b028ac83567b
Tenk1 <57358677+Tenk1@users.noreply.github.com>
2022-05-16 02:21:56 +0100

    Changed setup and nearly got it all working
    I changed it since last time now using class to get the basic setup which works better I did this after having a chat with Jacob the only thing that doesn't work is keeping credit the same once you replay it.

--- Roulette/Roulette/Roulette.cpp
+++ Roulette/Roulette/Roulette.cpp
@@ -19,66 +19,114 @@ void Start()
     cout << "Good luck \n" << endl;
 }
 
-/*
-  int add()
-  {
+class roulette
+{
+public:
+    int number, Pnumber;
+    string colour, Pcolour;
+    void printroulette()
+    {
+        cout << "Player guess\n";
+        cout << Pcolour << endl;
+        cout << Pnumber << endl;
 
-  }
+        cout << "Wheel total\n";
+        cout << colour << endl;
+        cout << number << endl;
+    }
+};
+roulette output[24], temp;
 
-  int sub()
-  {
+void printnumber()
+{
+    int i;
+    for (i = 0; i < 1; i++)
+    {
+        output[i].printroulette();
+    }
+}
 
-  }
 
- */
+void spin()
+{
+    srand(time(NULL));
+    int i, x;
+    for (i = 0; i < 1; i++)
+    {
+        x = rand() % 24;
+        temp = output[i];
+        output[i] = output[x];
+        output[x] = temp;
+    }
+}
+
+int Add(int a, int b)
+{
+    return (a + b);
+}
+
+int Sub(int a, int b)
+{
+    return (a - b);
+}
 
 int main()
 {
-    string colours[] = { "Red", "Black" };
-    int i;
-    int number[12] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-    int Credits = 100;
-    int Bet;
+   
     Start();
     Sleep(1000);
 
-Enterbet:
-    cout << "\n";
-    cout << "How much do you want to bet?\n";
-    cin >> Bet;
+    string colour[] = { "Red", "Black" };
+    int number[13] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
 
+    int Bet, i, o;
+
+
+    int Credits = 100;
+    int total;
+
+    total = Credits;
+placebet:
+    cout << "How much would you like to bet?\n";
+    cin >> Bet;
 
     if (Bet > Credits)
     {
-        cout << "Can't bet what you don't have";
-        goto Enterbet;
+        cout << "Can't bet what you don't have\n";
     }
 
-Entercolour:
-    cout << "\n";
-    cout << "What colour do you want to bet on?\n";
-    cout << "0: red \n" << "1: black \n";
+    cout << "what colour do you bet on?\n";
+    cout << "0:Red\n" << "1:Black\n";
     cin >> i;
-    cout << colours[i] << endl;
-    if (i > 2)
-    {
-        cout << "Red or Black only\n";
-        goto Entercolour;
-    }
 
-Enternumber:
-    cout << "\n";
     cout << "What number do you want to bet on?\n";
-    cout << "1 to 12\n";
-    for (int i = 0; i < 1; ++i)
+    cin >> o;
+
+
+    int x, y, z{};
+    for (x = 0; x < 2; x++)
     {
-        cin >> number[i];
-        cout << number[i] << endl;
-        if (number[i] > 13)
+        for (y = 0; y < 12; y++)
         {
-            cout << "Only 1 to 12";
-            goto Enternumber;
+            output[z].Pcolour = colour[i];
+            output[z].Pnumber = number[o];
+            output[z].colour = colour[x];
+            output[z].number = number[y];
+            z++;
         }
+    }
+    spin();
+    printnumber();
+    if (x == i || y == o)
+    {
+        total = Add(Credits, Bet);
+        cout << total << " " << "Credits\n";
+
+    }
+    else
+    {
+        total = Sub(Credits, Bet);
+        cout << total << " " << "Credits\n";
 
     }
 
