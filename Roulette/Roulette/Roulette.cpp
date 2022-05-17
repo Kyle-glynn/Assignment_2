ca87b586b476be96cf7f265ebe6ab8c72776bd1a
Tenk1 <57358677+Tenk1@users.noreply.github.com>
2022-05-17 02:11:03 +0100

    Issues
    I had issues with getting the player guess and total working together I got it working but I got rid of betting since it had issues now it is simple but works

--- Roulette/Roulette/Roulette.cpp
+++ Roulette/Roulette/Roulette.cpp
@@ -7,24 +7,31 @@
 using namespace std;
 
 
-
-
-
-void Start()
+void Start() //Intro to the game
 {
     cout << "Hello Welcome to roulette! \n";
-    cout << "You will have a 100 credits to start \n";
-    cout << "Your goal is to reach 1000 \n";
-    cout << "You can bet on a numebr up to 12 and whether it will be red or black \n";
-    cout << "Good luck \n" << endl;
+    cout << "You have a 100 credits\n";
+    cout << "You choose either red or black\n";
+    cout << "For the colour you think it will land on\n";
+    cout << "And you choose the number 0 to 12\n";
+    cout << "If you get one out of the two guesses right\n";
+    cout << "Like if you guess black and its black you win\n";
+    cout << "but if you guess wrong for both you lose\n";
+    cout << "Good luck\n" << endl;
 }
 
-class roulette
+class roulettenum //credits you start with
 {
 public:
-    int number, Pnumber;
+    int Credits = 100;
+};
+// I got help by 1000 'n' one passion https://www.youtube.com/watch?v=4bQdVyjvJig and https://www.youtube.com/watch?v=py7nc1Pjgto
+class roulette // Setup for roulette
+{
+public:  //wheel   //Player guess
+     int   number, Pnumber;
     string colour, Pcolour;
-    void printroulette()
+    void printwheel() // prints player guess and wheel total
     {
         cout << "Player guess\n";
         cout << Pcolour << endl;
@@ -34,127 +41,102 @@ public:
         cout << colour << endl;
         cout << number << endl;
     }
-};
-roulette output[24], temp;
 
-void printnumber()
+    void printinput()
+    {
+        roulettenum rounum; // ref of roulettenum
+
+            if (Pcolour == colour || Pnumber == number) // check if player guess and wheel total match
+            {
+                rounum.Credits += 25; // add 25 credits
+                cout << rounum.Credits << "Credits\n";
+            }
+            else // if not minus 25
+            {
+                rounum.Credits -= 25; //Takes away 25 credits
+                cout << rounum.Credits << "Credits\n";
+            }
+
+            if (rounum.Credits > 100) // credits more then 100 you win
+            {
+                cout << "You win\n";
+            }
+            else // below that you lose
+            {
+                cout << "You lose\n";
+            }
+        
+    }
+    
+};
+
+roulette output[24], temp; 
+void printnumber() // outputs both the wheel and ....
 {
     int i;
     for (i = 0; i < 1; i++)
     {
-        output[i].printroulette();
+        output[i].printwheel();
+        output[i].printinput();
     }
 }
 
 
-void spin()
+
+void spin() //random colour and number
 {
-    srand(time(NULL));
+    
     int i, x;
-    for (i = 0; i < 1; i++)
+    for (i = 0; i < 24; i++)
     {
-        x = rand() % 24;
+        srand(time(0)); // random seed
+        x = rand() % 24; // how many different  possibilities
         temp = output[i];
         output[i] = output[x];
         output[x] = temp;
     }
 }
 
-int Add(int a, int b)
+
+void game() // game output
 {
-    return (a + b);
-}
+    roulettenum rounum; // ref of roulettenum
 
-int Sub(int a, int b)
-{
-    return (a - b);
-}
+    string colour[] = { "Red", "Black" }; // array of colours
+    int number[13] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }; // array of numbers
+       
+    int Pc; // Player colour input 
+    int Pn; // Player number input
 
-void totalamount(int i)
-{
-    cout << i << " " << "Credits\n";
-}
-
-void game()
-{
-    string colour[] = { "Red", "Black" };
-    int number[13] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-
-    int Bet, i, o;
-
-    int Credits;
-
-    Credits = 100;
-
-
-    while (true)
-    {
-
-    cout << "How much would you like to bet?\n";
-    cin >> Bet;
-
-    if (Bet > Credits)
-    {
-        cout << "Can't bet what you don't have\n";
-    }
-
-    cout << "what colour do you bet on?\n";
-    cout << "0:Red\n" << "1:Black\n";
-    cin >> i;
-
-    cout << "What number do you want to bet on?\n";
-    cin >> o;
-
-    int x, y, z{};
-    for (x = 0; x < 2; x++)
-    {
-        for (y = 0; y < 13; y++)
-        {
-            output[z].Pcolour = colour[i];
-            output[z].Pnumber = number[o];
-            output[z].colour = colour[x];
-            output[z].number = number[y];
-            z++;
-        }
-    }
-    spin();
-    printnumber();
     
-    if (x == i || y == o)
-    {
-        Credits += Bet;
-        totalamount(Credits);
-    }
-    else if(x == i)
-    {
-        Credits += 25;
-        totalamount(Credits);
-    }
-    else if (y == o)
-    {
-        Credits += 25;
-        totalamount(Credits);
-    }
-    else if (x != i)
-    {
-        Credits -= Bet;
-        totalamount(Credits);
-    }
-    else if (y != o)
-    {
+        cout << "what colour do you bet on?\n";
+        cout << "0:Red\n" << "1:Black\n";
+        cin >> Pc;
+        cout << endl;
+        cout << "What number do you want to bet on?\n";
+        cin >> Pn;
 
-    }
-    {
-        Credits -= Bet;
-        totalamount(Credits);
-    }
-    }
+        int x, y, z{};
+        for (x = 0; x < 2; x++)
+        {
+            for (y = 0; y < 13; y++)
+            {
+                output[z].Pcolour = colour[Pc];
+                output[z].Pnumber = number[Pn];
+                output[z].colour = colour[x];
+                output[z].number = number[y];
+                z++;
+            }
+        }
+
+        spin();
+        printnumber();
 }
 
 int main()
 {
-    Start();
-    Sleep(1000);
-    game();
+    Start(); // output the intro
+    Sleep(1000); // delay for 1 second
+    game(); // output the game
 }
 
